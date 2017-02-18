7774f8a30b455d76880b7f44c53c1017a18d4829
Dan Sandiford <sonderfjord@gmail.com>
2017-02-18 16:48:46 +1100

    another model

--- underworld2/array.sh
+++ underworld2/array.sh
@@ -1,7 +1,16 @@
 counter=1
-for a in 0.0 0.2 0.4 0.6 0.8 1.0
+for a in 0.1 0.5 1.0
 do
-   #docker run -v $PWD:/workspace  -i -t --rm dansand/underworld2-dev mpirun -np 8 python isotropic.py A 2 $counter md.res=64 dp.a=$a dp.fa=30.0
-   echo $counter $a
-   let counter=counter+1
+   for b in 1.0 0.5 0.0
+   do
+      for c in 1.0 5.0 10.0
+      do
+         for d in 15.0 30.0
+         do 
+            #docker run -v $PWD:/workspace  -i -t --rm dansand/underworld2-dev mpirun -np 8 python isotropic.py E $counter md.res=64 dp.eta1*=$a dp.asthenosphere*=$b dp.U0*=$c dp.fa*=$d
+            echo $counter $a $b $c $d
+            let counter=counter+1
+         done
+      done
+   done
 done
