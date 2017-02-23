18792c074f041c7c998d3ed6e7510cb2295e8942
Dan Sandiford <sonderfjord@gmail.com>
2017-02-24 00:43:20 +1100

    and the array

--- underworld2/array.sh
+++ underworld2/array.sh
@@ -1,7 +1,16 @@
 counter=1
-for a in 0.0 0.2 0.4 0.6 0.8 1.0
+for a in 0.05 0.1 0.5 1.0
 do
-   #docker run -v $PWD:/workspace  -i -t --rm dansand/underworld2-dev mpirun -np 8 python isotropic.py C $counter md.res=64 dp.a=$a
-   echo $counter $a $b
-   let counter=counter+1
+   for b in 1.0 0.5 0.0
+   do
+      for c in 0.5 1.0 5.0
+      do
+         for d in 30.0
+         do
+            docker run -v $PWD:/workspace  -i -t --rm dansand/underworld2-dev mpirun -np 4 python isotropic.py G $counter md.res=64 dp.eta1*=$a dp.asthenosphere*=$b dp.U0*=$c dp.fa=$d
+            #echo $counter $a $b $c $d
+            let counter=counter+1
+         done
+      done
+   done
 done
