bb4f596037aa1726f16affbb54416dbd5c370837
Dan Sandiford <sonderfjord@gmail.com>
2017-03-06 15:12:19 +1100

    starting most of this again

--- underworld2/array.sh
+++ underworld2/array.sh
@@ -3,7 +3,9 @@ c=1.0
 for a in 1.0 0.8 0.6 0.4 0.2 0.1
 do
    b=$(echo "$c/$a" | bc -l)
-   docker run -v $PWD:/workspace  -i -t --rm dansand/underworld2-dev mpirun -np 4 python isotropic.py K $counter md.res=96 dp.eta1*=$a dp.eta2*=1.0 dp.U0*=$b
+   docker run -v $PWD:/workspace  -i -t --rm dansand/underworld2-dev mpirun -np 16 python isotropic.py A $counter md.res=96 dp.eta1*=$a dp.eta2*=$a dp.U0*=$b
+   #docker run -v $PWD:/workspace  -i -t --rm dansand/underworld2-dev mpirun -np 16 python isotropic.py B $counter md.res=96 dp.eta1*=$a dp.eta2*=1. dp.U0*=$b
+   #docker run -v $PWD:/workspace  -i -t --rm dansand/underworld2-dev mpirun -np 16 python isotropic.py C $counter md.res=96 dp.eta1*=1.0 dp.eta2*=$a dp.U0*=1.0
    #echo $a $b
    let counter=counter+1
 done
