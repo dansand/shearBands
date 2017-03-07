d9c4e3c6a42fb6de04d0daefc5d20ce7911e1ce3
Dan Sandiford <sonderfjord@gmail.com>
2017-03-07 17:47:49 +1100

    notchWidth pressure x2

--- underworld2/array.sh
+++ underworld2/array.sh
@@ -1,13 +1,14 @@
 counter=1
 c=1.0
-for a in 0.5 0.75 1.0 1.5 2.0
+for a in 0.5 1.0 2.0
 do
-   for b in 0.1 0.5 1.0 2.0 10.0
+   for b in 0.1 1.0 10.0
    do
       for c in 48 64 96
       do
-         docker run -v $PWD:/workspace  -i -t --rm dansand/underworld2-dev mpirun -np 16 python isotropic.py D $counter dp.notchWidth*=$a md.res=$c dp.eta2*=$b
-         #echo $a $b
+         #docker run -v $PWD:/workspace  -i -t --rm dansand/underworld2-dev mpirun -np 16 python isotropic.py D $counter dp.notchWidth*=$a md.res=$c dp.eta2*=$b
+         echo $counter $a $b $c
+         let counter=counter+1
       done
    done
 done
