6210e61f7d1028b5c4d2933932d3450a2adea534
Dan Sandiford <sonderfjord@gmail.com>
2017-03-21 22:19:17 +1100

    next cab

--- underworld2/array.sh
+++ underworld2/array.sh
@@ -1,12 +1,12 @@
 counter=1
 c=1.0
-for a in 0.5 1.0 2.0
+for a in 0. 0.1 0.5 1.0
 do
    for b in 0.0 10.0 20.0 30.0 40.0
    do
       for c in 64 96
       do
-         #docker run -v $PWD:/workspace  -i -t --rm dansand/underworld2-dev mpirun -np 16 python isotropic.py E $counter dp.notchWidth*=$a md.res=$c dp.fa*=$b
+         #docker run -v $PWD:/workspace  -i -t --rm dansand/underworld2-dev mpirun -np 16 python isotropic.py F $counter dp.asthenosphere*=$a md.res=$c dp.fa*=$b
          echo $counter $a $b $c
          let counter=counter+1
       done
